c23406956a2e5cd7bc9bac635fd8bb3f36fa4491
Peter Muys <peter@petermuys.com>
2016-11-23 07:11:44 +0100

    :construction: wip

---
+++ src/main/java/com/persistentbit/substema/ts/TsCodeGenOptions.java
@@ -0,0 +1,13 @@
+package com.persistentbit.substema.ts;
+
+import com.persistentbit.core.utils.BaseValueClass;
+
+/**
+ * Typescript Code generator options.
+ *
+ * @author petermuys
+ * @since 22/11/16
+ */
+public class TsCodeGenOptions extends BaseValueClass{
+
+}
